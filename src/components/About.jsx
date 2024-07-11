317d415eac94e7d146da0a07613cc9a76ff614ed
imanav10 <manavrj.07@gmail.com>
2024-07-11 19:09:50 +0530

    change

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,17 +1,51 @@
+import React, { useEffect } from 'react';
 import sample from '/Videos/video.mp4';
+import { motion } from 'framer-motion'; // Step 2: Import motion
 const About = () => {
-  return (
-    <div className="container">
-	  <video className='videoTag' autoPlay loop muted>
-		<section className="hero">
-			<h1>Hey</h1>
-		</section>
-		<source src={sample} type='video/mp4' />
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "//cdn.jsdelivr.net/github-cards/latest/widget.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []); // Empty dependency array means this effect runs once on mount
+
+	const grabCursorStyle = {
+	  cursor: 'grab',
+	};
+
+	return (
 		
+		<div>
+			<section className='hero'>
+			  <motion.div
+				className="github-card-wrapper"
+				drag
+				dragConstraints={{ left: 0, right: 300, top: 0, bottom: 300 }}
+				whileDrag={{ scale: 1.1 }}
+				style={grabCursorStyle} // Apply the grab cursor style here
+			  >
+				<div className="github-card" data-github="imanav10" data-width="400" data-height="150" data-theme="default">
+				  {/* Content that should be draggable along with the hand icon */}
+				</div>
+				<p>*pick me</p>
+			  </motion.div>
+			  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'white' }}>
+			{/* Adjust the styling as needed to ensure visibility */}
+			<h4>Hey Fellas</h4>
+		  </div>
+			</section>
+			<div className="video-container">
+				<video className='videoTag' autoPlay loop muted>
+					<source src={sample} type='video/mp4' />
+				</video>
+			</div>
+		</div>
+	);
+};
 
-	  </video>
-	</div>
-  )
-}
-
-export default About
+export default About;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,7 @@
 const Navbar = () => {
   return (
-    <div>
-        
-        <header className="w-full py-5 sm-px-10 px-5 flex justify-between items-center">
-            
+    <div style={{ backgroundColor: 'transparent' }}>
+        <header className="w-full py-5 sm-px-10 px-5 flex justify-between items-center" style={{ backgroundColor: 'transparent', important: true }}>
             <nav className="flex w-full screen-max-width">
                 <ul className="flex flex-1 justify-center">
                     <li className="px-10">@imanav10</li>
@@ -11,12 +9,11 @@ const Navbar = () => {
                     <li className="px-5 hover:text-gold transition-all"><a href="#experience">Experience</a></li>
                     <li className="px-5 hover:text-gold transition-all"><a href="#contact">Contact</a></li>
                 </ul>
-                
             </nav>
-            
         </header>
     </div>
   )
 }
 
 export default Navbar
+
